952c10b3b3485945d4b7858928a4fcf24c171864
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-08-10 13:21:12 +0200

    ....

--- angular-way/src/app/app.component.ts
+++ angular-way/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
+// decoratore like annotation 
 @Component({
+  // seleziona app-root nell html 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
