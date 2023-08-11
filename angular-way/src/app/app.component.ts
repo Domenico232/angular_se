302bda6c2f56dfee886f7934c996290479adb5ec
Domenico232 <123988959+Domenico232@users.noreply.github.com>
2023-08-11 16:46:49 +0200

    cards make

--- angular-way/src/app/app.component.ts
+++ angular-way/src/app/app.component.ts
@@ -8,10 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  arrayPokemon = [
+    {
+      nome: "Pikachu",
+      tipo: "Elettro",
+      mosse: ["Tuonopugno", "Fulmine", "Rapidopugno"],
+      image: "https://media.pokemoncentral.it/wiki/5/5e/Artwork0025_RFVF.png"
+    },
+    {
+      nome: "Charizard",
+      tipo: "Fuoco/Volante",
+      mosse: ["Lancio Fiamme", "Zuffa", "Volo"],
+      image: "https://media.pokemoncentral.it/wiki/4/48/Artwork0006_RFVF.png"
+    },
+    {
+      nome: "Squirtle",
+      tipo: "Acqua",
+      mosse: ["Pistolacqua", "Guscioforte", "Idropompa"],
+      image:"https://media.pokemoncentral.it/wiki/8/8d/Artwork0007_RFVF.png"
+    },
+    {
+      nome: "Bulbasaur",
+      tipo: "Erba/Veleno",
+      mosse: ["Foglielama", "Raffica Semina", "Velenpuntura"],
+      image:"https://media.pokemoncentral.it/wiki/9/9b/Artwork0001_RFVF.png"
+    },
+    {
+      nome: "Jigglypuff",
+      tipo: "Normale/Folletto",
+      mosse: ["Canto", "Ipnosi", "Pugno"],
+      image: "https://media.pokemoncentral.it/wiki/b/b5/Artwork0039_RFVF.png"
+    }
+  ];
+  
+  immaginepoke = "https://lorempokemon.fakerapi.it/pokemon/200"
+
+  isVisible=true;
+
   title = 'angular-way';
 
   onInput = (e : Event) =>{
-    console.log((<HTMLInputElement>e.target).value)
+   this.title = (<HTMLInputElement>e.target).value
+  }
+  onClick = (e: Event) =>{
+    this.isVisible = !this.isVisible;
+   this.title = "EZBUTTON"
   }
 
 }
--- angular-way/src/app/app.module.ts
+++ angular-way/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
@@ -26,6 +27,7 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule,
     MatButtonModule,
     MatInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
